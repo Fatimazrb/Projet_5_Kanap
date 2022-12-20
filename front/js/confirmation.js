f0f3783e5c319808fd5f46faeb0718457be11a1d
zarab fatima <zarab.fatima@gmail.com>
2022-12-20 15:58:21 +0100

    ajout des commits explicatiosn

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,18 +2,18 @@ const orderId = getOrderId ()
 displayOrderId(orderId)
 clearCache ()
 
-function getOrderId (){
+function getOrderId (){   // Récupération de l'orderId de notre commande situé dans le cart
     const queryString =  new URLSearchParams(window.location.search);
     const urlParams = queryString.get("orderId") 
     return urlParams
 }
 
-function displayOrderId (orderId){
+function displayOrderId (orderId){ // Affichage de l'orderId
     const Id = document.querySelector('#orderId')
     Id.innerHTML += orderId
 }
 
-function clearCache () {
+function clearCache () { // Effacement des produits dans le localStorage
     const cache = window.localStorage
     cache.clear
 }
